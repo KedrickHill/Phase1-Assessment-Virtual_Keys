f187abebe02a67176ebab40e98b13624326792d1
KedrickHill <kedrickjhill@gmail.com>
2021-02-15 14:47:21 -0600

    Added: A welcome to portion in main

--- VirtualKeys/src/main/java/VKeys/Menu.java
+++ VirtualKeys/src/main/java/VKeys/Menu.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Menu {
 
 int index = 0;
 
     public menu() {
+        //TODO: ask user for input on what they would like to do in the app
+        //TODO: using a switch to change the case would work here (Check with previous assignemnt: Bug Fixes)
         
+        Scanner input = new Scanner(System.in);
+        int option = input.nextInt();
+        switch (option) {
+            case 1:
+        }
     }
 }
--- VirtualKeys/src/main/java/VKeys/VkeysMain.java
+++ VirtualKeys/src/main/java/VKeys/VkeysMain.java
@@ -4,10 +4,19 @@ package VKeys;
  * Vkeys Main Workbench
  *
  */
-public class VKeysMain 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class VKeysMain {
+    public static void main(String[] args) {
+        // TODO: add a welcome section at the beginning of application to welcome the
+        // user
+
+        System.out.println("\n****************************************************************\n");
+        System.out.println("WELCOME TO LOCKEDME.COM");
+        System.out.println("\n****************************************************************\n");
+
+        // TODO: set up menu so a user can move throughout it.
+        // TODO: each menu should be unavailble and should then allow the user to return
+        // to the menu
+        // TODO: Code should not be very long here.
+        System.out.println("Hello World!");
     }
 }
