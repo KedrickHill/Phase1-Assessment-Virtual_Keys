4482226b77aab3b30122f26287993cd5d1659086
KedrickHill <kedrickjhill@gmail.com>
2021-02-24 12:19:46 -0600

    Update to Menu: incorporates a sub menu; formatting changes

--- VirtualKeys/src/main/java/VKeys/Menu.java
+++ VirtualKeys/src/main/java/VKeys/Menu.java
@@ -5,67 +5,87 @@ import java.util.Scanner;
 
 public class Menu {
 
-    int index = 0;
-    final static String PATH = System.getProperty("user.dir") + "/MockDir";
+    // Working directory
+    static Directory dir = new Directory(new File(System.getProperty("user.dir") + "/MockDir"));
 
     public static void menu() {
 
         Scanner input = new Scanner(System.in);
-        Directory dir = new Directory(new File(PATH));
+        System.out.println("***** MAIN MENU *****");
         System.out.println(
-                "1- Display Current Directory\n2- Show All Files\n3- Add a File\n4- Delete a File\n5- Delete All Files\n6- Search for a File\n7- Exit LockedMe\n");
+                "1- Show All Files\n2- File Management\n3- Exit LockedMe\n");
 
         System.out.println("Enter a Choice:");
         int option = input.nextInt();
         switch (option) {
-            case 1: // display current directory
-                System.out.println("\nYour Current Directory: " + dir.getDir() + "\n");
-                menu();
-                break;
-
-            case 2: // show all files in directory
+            case 1: // show all files in directory
                 System.out.println("\nFiles in Directory: " + dir.showAllFiles() + "\n");
                 menu();
                 break;
+            
+            case 2: // move to File Management processes
+                subMenu();
+                break;
 
-            case 3: // add a file to the directory
+            case 3: //exit the application
+                System.out.println("\nClosing Application...\n");
+                break;
+
+            default: // closes application if not a 1-3 option
+                System.out.println("\nInvalid choice! Closing Application...\n");
+                break;
+        }
+        input.close();
+    }
+
+
+    public static void subMenu() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n***** FILE MANAGEMENT *****");
+        System.out.println("1- Add a File\n2- Delete a File\n3- Delete All Files\n4- Search for a File\n5- Return to Main Menu\n");
+        System.out.println("Enter a Choice:");
+        int option = input.nextInt();
+        switch(option) {
+            case 1: // adds a file to current directory
                 System.out.println("\nEnter the files name and type:");
                 input.nextLine();
                 String add = input.nextLine();
                 dir.addFile(add);
                 System.out.println("\nFile has been created\n");
-                menu();
+                subMenu();
                 break;
 
-            case 4: // delete a file from directory
+            case 2: // delete a file from the directory
                 System.out.println("\nEnter a file to be deleted:");
                 input.nextLine();
                 String del = input.nextLine();
                 dir.deleteFile(del);
                 System.out.println("\nFile has been deleted\n");
-                menu();
+                subMenu();
                 break;
 
-            case 5: // delete all files from directory?
+            case 3: // delete all files in the directory
                 System.out.println("\nOh! You were thinking about it! To bad.\n");
-                menu();
+                subMenu();
                 break;
 
-            case 6: // search the directory for a file
+            case 4: // search for a file in the directory
                 System.out.println("\nEnter a file to search:");
                 input.nextLine();
                 String srch = input.nextLine();
                 dir.searchFile(srch);
+                subMenu();
+                break;
+
+            case 5: // return to the main menu
+                System.out.println("\nReturning to Main Menu...\n");
                 menu();
                 break;
 
-            case 7: //exit the application
-                System.out.println("\nClosing Application...\n");
+            default:
+                System.out.println("\nInvalid Choice! Try Again.");
+                subMenu();
                 break;
-            default: // asks for
-                System.out.println("\nInvalid choice! Closing Application...\n");
-                break;
-
         }
         input.close();
     }
