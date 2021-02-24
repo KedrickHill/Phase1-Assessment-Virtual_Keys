e468f97eccb33c1036abf5792f4196ae7fabb50e
KedrickHill <kedrickjhill@gmail.com>
2021-02-24 11:46:40 -0600

    Update to welcome screen adding UI directives

--- VirtualKeys/src/main/java/VKeys/VkeysMain.java
+++ VirtualKeys/src/main/java/VKeys/VkeysMain.java
@@ -10,8 +10,10 @@ public class VkeysMain {
         System.out.println("\n****************************************************************\n");
         System.out.println("WELCOME TO LOCKEDME.COM");
         System.out.println("\n****************************************************************\n");
-        System.out.println(
-                "Use numerical values for pathing along routes. \n**DISCLOSURE: Not all functionality is implemented. Denoted with a * character.**\n");
+        System.out.println("Use numerical values for pathing along routes.");
+        System.out.println("Selecting option 1 will display all the current files in the directory");
+        System.out.println("Selecting option 2 brings you to file managment where you can create, delete, and search files.");
+        System.out.println("Selecting option 3 or a invalid choice will close the application.\n");
         Menu.menu();
 
     }
